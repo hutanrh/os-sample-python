80b621ee62dadf88d7cae7907c110db31d6a13cf
hutanrh <39288870+hutanrh@users.noreply.github.com>
2018-05-15 18:53:01 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,33 @@
-from flask import Flask
-application = Flask(__name__)
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
-@application.route("/")
-def hello():
-    return "Hello Word!"
+app = dash.Dash()
+application = app.server
 
-if __name__ == "__main__":
-    application.run()
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+if __name__ == '__main__':
+    app.run_server(port=8080, debug=True)
